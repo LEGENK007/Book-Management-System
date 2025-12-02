8ee27798652b7fe50f097cc6b8f64b8c7e76a434
Rajesh <rajeshn387@gmail.com>
2025-12-03 01:49:31 +0530

    Added crud service for books, user and reviews

---
+++ app/db/crud/users.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from typing import Optional
+
+from app.models.models import User
+from app.schemas.user import UserCreate
+
+# Get user by email
+async def get_user_by_email(session: AsyncSession, email: str) -> Optional[User]:
+    result = await session.execute(
+        select(User).where(User.email == email)
+    )
+    return result.scalar_one_or_none()
+
+
+# Create user
+async def create_user(session: AsyncSession, user_in: UserCreate, password_hash: str) -> User:
+    new_user = User(
+        email = user_in.email,
+        password = password_hash,
+        role = user_in.role,
+    )
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
